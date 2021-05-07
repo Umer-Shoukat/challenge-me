e20b5075767aa75d6ae1bb8866d3d929bc8cf659
M Izhan <izhan@softsquare.io>
2021-05-08 00:29:43 +0500

    remove the cordova and simple vue app

--- cordova-app/src/config/config.js
+++
@@ -1,1 +0,0 @@
-export const apiEndPoint = "http://localhost:3000/api/v1";
--- web-app/src/plugins/socketio.js
+++
@@ -1,6 +0,0 @@
-import VueSocketIO from "vue-socket.io";
-
-export default new VueSocketIO({
-  debug: true,
-  connection: "http://localhost:3000/",
-});
